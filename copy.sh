46d758a05683a412b0196504eea8a18ae63b1b24
bernardcjason@gmail.com <bernardcjason@gmail.com>
2020-05-02 07:37:55 +0100

    updates needed after so many years

--- copy.sh
+++ copy.sh
@@ -1,9 +1,4 @@
-cp android/screen.png   ./platforms/android/res/drawable-port-mdpi/screen.png
-cp android/screen.png   ./platforms/android/res/drawable-port-ldpi/screen.png
-cp android/screen.png   ./platforms/android/res/drawable-port-hdpi/screen.png
-cp android/screen.png   ./platforms/android/res/drawable-port-xhdpi/screen.png
-
-cp android/mipmap-hdpi/icon.png ./platforms/android/res/mipmap-hdpi/icon.png
-cp android/mipmap-ldpi/icon.png ./platforms/android/res/mipmap-ldpi/icon.png
-cp android/mipmap-mdpi/icon.png ./platforms/android/res/mipmap-mdpi/icon.png
-cp android/mipmap-xhdpi/icon.png ./platforms/android/res/mipmap-xhdpi/icon.png
+for i in `find platforms -name screen.png`
+do
+cp android/screen.png $i
+done
